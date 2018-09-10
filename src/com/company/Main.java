f3360f87068144c2dca95faa1eab5458a2eb262f
Your GITHUB Name <GITHUB you@example.com>
2018-09-10 11:00:28 +0300

    ver2

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ф.И.О - Горбатюк Сергей Николаевич");
+        System.out.print("Возраст - 19 лет\n");
+        System.out.println("3-курс");
+        System.out.println("AT -  161");
+        System.out.println("№ по спии\bску - 3");
+        System.out.println("\tИ так дальше!!!");
+    }
+}
